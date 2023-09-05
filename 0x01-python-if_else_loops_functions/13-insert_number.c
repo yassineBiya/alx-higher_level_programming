3bc6591443cc4cdf7f129edd3d9a5948ef7bf54f
Yassine Biyaali <melbiyaali@gmail.com>
2023-09-05 15:15:47 -0700

    print recursive

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,13 +1,13 @@
 #include "lists.h"
+
 /**
- * insert_node - Inserts a number into a sorted SLL.
+ * insert_node - Inserts a number into a sorted singly-linked list.
  *
- * @head: A pointer the head of the LL.
+ * @head: A pointer the head of the linked list.
  * @number: The number to insert.
  *
- * Return: if fails - NULL or pointer to new.
+ * Return: NULL if fails or pointer to new.
  */
- 
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node = *head, *new;
