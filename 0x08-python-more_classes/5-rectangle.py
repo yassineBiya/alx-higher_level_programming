5b793b9ca7be46458dec2ce7b26299a6f84aefaf
Yassine Biyaali <melbiyaali@gmail.com>
2023-10-29 09:09:41 -0700

    update

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,5 +1,4 @@
-# 5-rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
+#!/usr/bin/python3
 """Defines a Rectangle class."""
 
 
@@ -8,6 +7,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
+
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -53,6 +53,7 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
+
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
