3ab2a4c667806c28da92afb0c1d478abb01f413d
Your Name <you@example.com>
2024-04-02 12:41:29 +0000

    list safe printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,6 @@
-afe_print_list(my_list=[], x=0):
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
     Args:
         my_list (list): The list to print elements from.
